8d53d6e07fe6ce093ace187c6b49137a3d924ba4
Travis Swicegood <development@domain51.com>
2010-02-05 21:16:22 -0600

    use new super-class run() for bootstrapping

--- d51/django/virtualenv/shell/__init__.py
+++ d51/django/virtualenv/shell/__init__.py
@@ -4,11 +4,7 @@ pkg_resources.declare_namespace(__name__)
 from d51.django.virtualenv.base import VirtualEnvironment
 
 class VirtualEnvironmentShell(VirtualEnvironment):
-    def run_shell(self, my_settings):
-        self.activate()
-        if hasattr(self.caller, 'setUp'):
-            self.caller.setUp()
-
-        self.configure_settings(my_settings)
+    def run(self, my_settings):
+        super(VirtualEnvironmentShell, self).run(my_settings)
         self.call_command('shell')
 
